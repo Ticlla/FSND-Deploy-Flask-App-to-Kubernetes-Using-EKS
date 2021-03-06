318e3be94b70671ffa60d46a001ff993691910a7
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2021-03-05 22:00:11 -0400

    new change

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy page de los cumpas")
 
 
 @APP.route('/auth', methods=['POST'])
